e7b77cd3954d07ac4822d177e3a183d95b518396
Denis Rechkunov <mail@pragmader.me>
2015-02-12 15:54:33 +0600

    Change API to Catberry 4.0.

--- test/lib/LocalizationLoader.js
+++ test/lib/LocalizationLoader.js
@@ -31,18 +31,49 @@
 'use strict';
 
 var assert = require('assert'),
+	events = require('events'),
+	Promise = require('promise'),
 	http = require('http'),
 	path = require('path'),
 	LocalizationLoader = require('../../lib/LocalizationLoader'),
 	Logger = require('../mocks/Logger'),
 	ServiceLocator = require('catberry-locator');
 
+global.Promise = Promise;
+
+var caseRoot = path.join(
+	__dirname, '..', 'cases', 'lib', 'server', 'LocalizationLoader'
+);
+
 var defaultLocale = 'ru',
+	components = {
+		component1: {
+			path: path.join(
+				caseRoot, 'components', 'component1', 'test-comp.json'
+			)
+		},
+		component2: {
+			path: path.join(
+				caseRoot, 'components', 'component2', 'test-comp.json'
+			)
+		},
+		// not exists
+		component3: {
+			path: path.join(
+				caseRoot, 'components', 'component3', 'test-comp.json'
+			)
+		},
+		// not a directory
+		component4: {
+			path: path.join(
+				caseRoot, 'components', 'component4', 'test-comp.json'
+			)
+		}
+	},
 	defaultConfig = {
 		l10n: {
 			defaultLocale: defaultLocale,
-			path: path.join(__dirname, '..', 'cases',
-				'lib', 'server', 'LocalizationLoader')
+			path: caseRoot
 		}
 	},
 	localizations = {
@@ -74,16 +105,35 @@ describe('server/LocalizationLoader', function () {
 				}, 'Error expected');
 			});
 
-		it('should throw exception when default localization can not be loaded',
+		it('should throw exception when default locale is wrong',
 			function () {
 				assert.throws(function () {
-					var locator = createLocator();
-					locator.resolveInstance(LocalizationLoader, {
-						l10n: {
-							defaultLocale: 'ch'
-						}
-					});
+					var config = {
+							l10n: {
+								defaultLocale: 'china'
+							}
+						},
+						locator = createLocator(config);
+					locator.resolveInstance(LocalizationLoader, config);
 				}, 'Error expected');
+			});
+
+		it('should throw exception when default localization can not be loaded',
+			function (done) {
+				var locator = createLocator(),
+					eventBus = locator.resolve('eventBus');
+				eventBus.on('error', function (error) {
+					assert.strictEqual(
+						error.message, 'Can not load default locale ch'
+					);
+					done();
+				});
+				locator.resolveInstance(LocalizationLoader, {
+					l10n: {
+						defaultLocale: 'ch'
+					}
+				});
+				eventBus.emit('allComponentsLoaded');
 			});
 
 		it('should not throw exception when default locale is specified',
@@ -98,8 +148,9 @@ describe('server/LocalizationLoader', function () {
 	describe('#load', function () {
 		it('should throw exception on wrong locale', function () {
 			var locator = createLocator(),
-				loader = locator.resolveInstance(LocalizationLoader,
-					defaultConfig);
+				loader = locator.resolveInstance(
+					LocalizationLoader, defaultConfig
+				);
 
 			assert.throws(function () {
 				loader.load('wrong');
@@ -107,108 +158,158 @@ describe('server/LocalizationLoader', function () {
 		});
 
 		it('should return default localization when argument is default locale',
-			function () {
+			function (done) {
 				var locator = createLocator(),
-					loader = locator.resolveInstance(LocalizationLoader,
-						defaultConfig),
-					localization = loader.load(defaultLocale);
+					eventBus = locator.resolve('eventBus'),
+					loader = locator.resolveInstance(
+						LocalizationLoader, defaultConfig
+					);
 
-				assert.deepEqual(localization, localizations[defaultLocale],
-					'Localization do not match'
-				);
+				eventBus
+					.on('error', done)
+					.on('l10nLoaded', function () {
+						var localization = loader.load(defaultLocale);
+						assert.deepEqual(
+							localization, localizations[defaultLocale],
+							'Localization do not match'
+						);
+						done();
+					});
+				eventBus.emit('allComponentsLoaded');
 			});
 
 		it('should return non-default localization merged with default',
-			function () {
+			function (done) {
 				var locator = createLocator(),
-					loader = locator.resolveInstance(LocalizationLoader,
-						defaultConfig),
-					enLocalization = localizations.en,
-					localization = loader.load('en');
+					eventBus = locator.resolve('eventBus'),
+					loader = locator.resolveInstance(
+						LocalizationLoader, defaultConfig
+					);
 
-				assert.strictEqual(
-					localization.FIRST_VALUE, enLocalization.FIRST_VALUE,
-					'Localization do not match'
-				);
-				assert.strictEqual(
-					localization.SECOND_VALUE, enLocalization.SECOND_VALUE,
-					'Localization do not match'
-				);
-				assert.strictEqual(
-					localization.THIRD_VALUE, defaultLocalization.THIRD_VALUE,
-					'Localization do not match'
-				);
+				eventBus
+					.on('error', done)
+					.on('l10nLoaded', function () {
+						var enLocalization = localizations.en,
+							localization = loader.load('en');
+
+						assert.strictEqual(
+							localization.FIRST_VALUE,
+							enLocalization.FIRST_VALUE,
+							'Localization do not match'
+						);
+						assert.strictEqual(
+							localization.SECOND_VALUE,
+							enLocalization.SECOND_VALUE,
+							'Localization do not match'
+						);
+						assert.strictEqual(
+							localization.THIRD_VALUE,
+							defaultLocalization.THIRD_VALUE,
+							'Localization do not match'
+						);
+						done();
+					});
+				eventBus.emit('allComponentsLoaded');
 			});
 
 		it('should return localization merged with default using full name',
-			function () {
+			function (done) {
 				var locator = createLocator(),
-					loader = locator.resolveInstance(LocalizationLoader,
-						defaultConfig),
-					enUsLocalization = localizations['en-us'],
-					localization = loader.load('en-us');
+					eventBus = locator.resolve('eventBus'),
+					loader = locator.resolveInstance(
+						LocalizationLoader, defaultConfig
+					);
 
-				assert.strictEqual(
-					localization.FIRST_VALUE, enUsLocalization.FIRST_VALUE,
-					'Localization do not match'
-				);
-				assert.strictEqual(
-					localization.SECOND_VALUE, enUsLocalization.SECOND_VALUE,
-					'Localization do not match'
-				);
-				assert.strictEqual(
-					localization.THIRD_VALUE, defaultLocalization.THIRD_VALUE,
-					'Localization do not match'
-				);
-				assert.strictEqual(
-					localization.FOURTH_VALUE, enUsLocalization.FOURTH_VALUE,
-					'Localization do not match'
-				);
-				assert.strictEqual(
-					localization.FIFTH_VALUE, enUsLocalization.FIFTH_VALUE,
-					'Localization do not match'
-				);
+				eventBus
+					.on('error', done)
+					.on('l10nLoaded', function () {
+						var enUsLocalization = localizations['en-us'],
+							localization = loader.load('en-us');
+
+						assert.strictEqual(
+							localization.FIRST_VALUE,
+							enUsLocalization.FIRST_VALUE,
+							'Localization do not match'
+						);
+						assert.strictEqual(
+							localization.SECOND_VALUE,
+							enUsLocalization.SECOND_VALUE,
+							'Localization do not match'
+						);
+						assert.strictEqual(
+							localization.THIRD_VALUE,
+							defaultLocalization.THIRD_VALUE,
+							'Localization do not match'
+						);
+						assert.strictEqual(
+							localization.FOURTH_VALUE,
+							enUsLocalization.FOURTH_VALUE,
+							'Localization do not match'
+						);
+						assert.strictEqual(
+							localization.FIFTH_VALUE,
+							enUsLocalization.FIFTH_VALUE,
+							'Localization do not match'
+						);
+						done();
+					});
+				eventBus.emit('allComponentsLoaded');
 			});
 
-		it('should return localization merged with modules localization',
-			function () {
-				var locator = createLocator(),
-					config = Object.create(defaultConfig);
-				config.modulesFolder = path.join(__dirname, '..', 'cases',
-					'lib', 'server', 'LocalizationLoader', 'modules');
-				var loader = locator.resolveInstance(LocalizationLoader,
-						config),
-					localization = loader.load('en-us');
+		it('should return localization merged with components localization',
+			function (done) {
+				var locator = createLocator(components),
+					eventBus = locator.resolve('eventBus'),
+					loader = locator.resolveInstance(
+						LocalizationLoader, defaultConfig
+					);
 
-				var expectedLocalization = {
-					FIRST_VALUE: 'en-us locale first by module1',
-					SECOND_VALUE: 'en-us locale second',
-					THIRD_VALUE: 'ru locale third',
-					FOURTH_VALUE: 'en-us locale fourth by module2',
-					FIFTH_VALUE: 'en-us locale fifth',
-					SIXTH_VALUE: 'en-us locale sixth by module1',
-					SEVENTH_VALUE: 'en-us locale seventh by module2',
-					EIGHTH_VALUE: 'en-us locale eighth by module2',
-					$pluralization: localizations['en-us'].$pluralization
-				};
+				eventBus
+					.on('error', done)
+					.on('l10nLoaded', function () {
+						var localization = loader.load('en-us');
 
-				assert.deepEqual(localization, expectedLocalization,
-					'Localization do not match'
-				);
+						var expectedLocalization = {
+							FIRST_VALUE: 'en-us locale first by module1',
+							SECOND_VALUE: 'en-us locale second',
+							THIRD_VALUE: 'ru locale third',
+							FOURTH_VALUE: 'en-us locale fourth by module2',
+							FIFTH_VALUE: 'en-us locale fifth',
+							SIXTH_VALUE: 'en-us locale sixth by module1',
+							SEVENTH_VALUE: 'en-us locale seventh by module2',
+							EIGHTH_VALUE: 'en-us locale eighth by module2',
+							$pluralization: localizations['en-us'].$pluralization
+						};
+
+						assert.deepEqual(localization, expectedLocalization,
+							'Localization do not match'
+						);
+						done();
+					});
+				eventBus.emit('allComponentsLoaded');
 			}
 		);
 
 		it('should return same localization on short or full name',
-			function () {
-				var locator = createLocator(),
-					loader = locator.resolveInstance(LocalizationLoader,
-						defaultConfig),
-					enLocalization = loader.load('en'),
-					enGbLocalization = loader.load('en-gb');
+			function (done) {
+				var locator = createLocator(components),
+					eventBus = locator.resolve('eventBus'),
+					loader = locator.resolveInstance(
+						LocalizationLoader, defaultConfig
+					);
 
-				assert.deepEqual(enLocalization, enGbLocalization,
-					'Localization do not match'
-				);
+				eventBus
+					.on('error', done)
+					.on('l10nLoaded', function () {
+						var enLocalization = loader.load('en'),
+							enGbLocalization = loader.load('en-gb');
+
+						assert.deepEqual(enLocalization, enGbLocalization,
+							'Localization do not match'
+						);
+						done();
+					});
+				eventBus.emit('allComponentsLoaded');
 			});
 	});
 
@@ -400,68 +501,201 @@ describe('server/LocalizationLoader', function () {
 		it('should return localization file using cookie locale',
 			function (done) {
 				var locator = createLocator(),
+					eventBus = locator.resolve('eventBus'),
 					loader = locator.resolveInstance(
-						LocalizationLoader, defaultConfig),
+						LocalizationLoader, defaultConfig
+					),
 					server = createServer(loader.getMiddleware());
 
-				server.listen(8084, function () {
-					var request = http.request({
-							port: 8084,
-							agent: false,
-							path: '/l10n.js',
-							headers: {
-								Cookie: 'locale=en-us'
-							}
-						},
-						function (response) {
-							assert.strictEqual(response.statusCode, 200,
-								'Wrong status');
+				var enUsLocalization = localizations['en-us'],
+					mergedLocalization = {};
+				mergedLocalization.FIRST_VALUE =
+					enUsLocalization.FIRST_VALUE;
+				mergedLocalization.SECOND_VALUE =
+					enUsLocalization.SECOND_VALUE;
+				mergedLocalization.THIRD_VALUE =
+					defaultLocalization.THIRD_VALUE;
+				mergedLocalization.FOURTH_VALUE =
+					enUsLocalization.FOURTH_VALUE;
+				mergedLocalization.FIFTH_VALUE =
+					enUsLocalization.FIFTH_VALUE;
+				mergedLocalization.$pluralization =
+					localizations['en-us'].$pluralization;
 
-							var enUsLocalization = localizations['en-us'],
-								mergedLocalization = {};
-							mergedLocalization.FIRST_VALUE =
-								enUsLocalization.FIRST_VALUE;
-							mergedLocalization.SECOND_VALUE =
-								enUsLocalization.SECOND_VALUE;
-							mergedLocalization.THIRD_VALUE =
-								defaultLocalization.THIRD_VALUE;
-							mergedLocalization.FOURTH_VALUE =
-								enUsLocalization.FOURTH_VALUE;
-							mergedLocalization.FIFTH_VALUE =
-								enUsLocalization.FIFTH_VALUE;
-							mergedLocalization.$pluralization =
-								localizations['en-us'].$pluralization;
-
-							var data = '';
-
-							response.setEncoding('utf8');
-							response.on('data', function (chunk) {
-								data += chunk;
-							});
-							response.on('end', function () {
-								var window = {};
-								/*jshint evil:true */
-								eval(data);
-								assert.deepEqual(
-									window.localization, mergedLocalization,
-									'Localization do not match'
+				eventBus
+					.on('error', done)
+					.on('l10nLoaded', function () {
+						var request = http.request({
+								port: 8084,
+								agent: false,
+								path: '/l10n.js',
+								headers: {
+									Cookie: 'locale=en-us'
+								}
+							},
+							function (response) {
+								assert.strictEqual(
+									response.statusCode, 200, 'Wrong status'
 								);
-								server.close(function () {
-									done();
-								});
-							});
-						});
 
-					request.end();
+								var data = '';
+
+								response.setEncoding('utf8');
+								response
+									.on('data', function (chunk) {
+										data += chunk;
+									})
+									.on('end', function () {
+										var window = {};
+										/*jshint evil:true */
+										eval(data);
+										assert.deepEqual(
+											window.localization,
+											mergedLocalization,
+											'Localization do not match'
+										);
+										server.close(function () {
+											done();
+										});
+									});
+							});
+						request.end();
+					});
+				server.listen(8084, function () {
+					eventBus.emit('allComponentsLoaded');
 				});
 			});
 
+		it('should return localization file using cookie short locale',
+			function (done) {
+				var locator = createLocator(),
+					eventBus = locator.resolve('eventBus'),
+					loader = locator.resolveInstance(
+						LocalizationLoader, defaultConfig
+					),
+					server = createServer(loader.getMiddleware());
+
+				var enLocalization = localizations.en,
+					mergedLocalization = {};
+				mergedLocalization.FIRST_VALUE =
+					enLocalization.FIRST_VALUE;
+				mergedLocalization.SECOND_VALUE =
+					enLocalization.SECOND_VALUE;
+				mergedLocalization.THIRD_VALUE =
+					defaultLocalization.THIRD_VALUE;
+				mergedLocalization.$pluralization =
+					localizations.en.$pluralization;
+
+				eventBus
+					.on('error', done)
+					.on('l10nLoaded', function () {
+						var request = http.request({
+								port: 8085,
+								agent: false,
+								path: '/l10n.js',
+								headers: {
+									Cookie: 'locale=en-au'
+								}
+							},
+							function (response) {
+								assert.strictEqual(
+									response.statusCode, 200, 'Wrong status'
+								);
+
+								var data = '';
+
+								response.setEncoding('utf8');
+								response
+									.on('data', function (chunk) {
+										data += chunk;
+									})
+									.on('end', function () {
+										var window = {};
+										/*jshint evil:true */
+										eval(data);
+										assert.deepEqual(
+											window.localization,
+											mergedLocalization,
+											'Localization do not match'
+										);
+										server.close(function () {
+											done();
+										});
+									});
+							});
+						request.end();
+					});
+				server.listen(8085, function () {
+					eventBus.emit('allComponentsLoaded');
+				});
+			});
+
+		it('should return default localization file using cookie wrong locale',
+			function (done) {
+				var locator = createLocator(),
+					eventBus = locator.resolve('eventBus'),
+					loader = locator.resolveInstance(
+						LocalizationLoader, defaultConfig
+					),
+					server = createServer(loader.getMiddleware());
+
+				eventBus
+					.on('error', done)
+					.on('l10nLoaded', function () {
+						var request = http.request({
+								port: 8086,
+								agent: false,
+								path: '/l10n.js',
+								headers: {
+									Cookie: 'locale=ch'
+								}
+							},
+							function (response) {
+								assert.strictEqual(
+									response.statusCode, 200, 'Wrong status'
+								);
+
+								var data = '';
+
+								response.setEncoding('utf8');
+								response
+									.on('data', function (chunk) {
+										data += chunk;
+									})
+									.on('end', function () {
+										var window = {};
+										/*jshint evil:true */
+										eval(data);
+										assert.deepEqual(
+											window.localization,
+											defaultLocalization,
+											'Localization do not match'
+										);
+										server.close(function () {
+											done();
+										});
+									});
+							});
+						request.end();
+					});
+				server.listen(8086, function () {
+					eventBus.emit('allComponentsLoaded');
+				});
+			});
 	});
 });
 
-function createLocator() {
+function createLocator(components) {
+	components = components || {};
 	var locator = new ServiceLocator();
 	locator.register('logger', Logger);
+	locator.registerInstance('serviceLocator', locator);
+	locator.registerInstance('componentLoader', {
+		getComponentsByNames: function () {
+			return components;
+		}
+	});
+	locator.registerInstance('eventBus', new events.EventEmitter());
 	return locator;
 }
 
